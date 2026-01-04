e5bc006322e5f9d614c931b6059fc6c315a870d4
cheikhdiallo242-cpu <cheikhdiallo242@gmail.com>
2026-01-04 17:39:44 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
 from flask import Flask, request, jsonify, render_template
-import os
-from openai import OpenAI
+from brain import think
 
 app = Flask(__name__)
-
-client = OpenAI(
-    api_key=os.environ.get("OPENAI_API_KEY")
-)
 
 @app.route("/")
 def home():
